78e2b79edac9108aad9f5bc32c7b40dc667b7327
unknown <ortiztato@gmail.com>
2022-06-10 13:13:47 -0600

    mas estilos y bug del watchlist

--- auctions/static/app.js
+++ auctions/static/app.js
@@ -8,3 +8,5 @@ function toggleMenu() {
 
 openMenuBtn.addEventListener("click", toggleMenu);
 closeMenuBtn.addEventListener("click", toggleMenu);
+
+function alertlogin () {alert ("You need to sign in first")}
